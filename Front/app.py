5386f8474d5465e58cabef8118a76c82f963edaf
salah127 <salah1999jari@gmail.com>
2025-05-15 23:35:17 +0200

    ss

--- Front/app.py
+++ Front/app.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
- 
+# MongoDB connection
 # Connect to MongoDB
 client = MongoClient('mongodb://localhost:27017/')
 db = client['article_db']
